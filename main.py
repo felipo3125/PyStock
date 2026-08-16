99f6a1365b6abcd9922fbace9815d8168f6b8e59
Felipe Valdivieso <felipevaldivieso17@gmail.com>
2026-08-16 18:55:02 +0200

    Initial PyStock proyect

---
+++ main.py
@@ -0,0 +1,17 @@
+#programa pystock
+
+
+
+print("""========================
+        PyStock
+========================
+
+1. Ver productos
+2. Añadir producto
+3. Buscar producto
+4. Modificar producto
+5. Eliminar producto
+6. Estadísticas
+7. Salir
+
+Selecciona una opción::""")
