9dc65ff0d25b36586e827523141c707717443e51
Felipe Valdivieso <felipevaldivieso17@gmail.com>
2026-08-17 20:24:16 +0200

    Add product update

--- main.py
+++ main.py
@@ -52,7 +52,51 @@ def add_product(name, category, price, stock):
 
 def search_product(name):
      product = view_products()
-     product["name"] = name
+     for i in product:
+          if i["name"] == name:
+               print(i)
+
+def update_product():
+     products = view_products()
+
+     found = False
+
+     id = int(input("please enter your ID number: "))
+     for i in products:
+          if id == i["id"]:
+               found = True
+               print("ID found")
+               print(i)
+               print("""which section do you want to change?
+                         1. name
+                         2. category
+                         3. price
+                         4. stock
+                         5. salir""")
+               while True:
+                    option = int(input("option: "))
+                    if option == 1:
+                         i["name"] = input("new name: ")
+                         print("Name successfully changed")
+                    elif option == 2:
+                         i["category"] = input("new category: ")
+                         print("Category successfully changed")
+                    elif option == 3:
+                         i["price"] = input("new price: ")
+                         print("Price successully changed")
+                    elif option == 4:
+                         i["stock"] = input("new stock: ")
+                         print("Stock successfully changed")
+                    elif option == 5:
+                         print("see ya")
+                         break
+                    else:
+                         print("invalid option")
+               save_products(products)
+     if not found:
+          print("product not found")
+
+          
 
 print("""========================
         PyStock
